645e8cb7b55b93f6656fe64cbb4d529b56a3b5b4
DavidStahl97 <david.stahl97@outlook.com>
2021-01-30 16:24:01 +0100

    customer validation

--- PizzaBlazor/Shared/Customer.cs
+++ PizzaBlazor/Shared/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,16 @@ namespace PizzaBlazor.Shared
     {
         public int Id { get; init; }
 
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50)]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Street is required")]
+        [StringLength(50)]
         public string Street { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50)]
         public string City { get; set; }
     }
 }
