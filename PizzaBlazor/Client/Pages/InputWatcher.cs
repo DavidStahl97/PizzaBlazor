41f00dd45f50cbe4af0256a1b2406a48339c943f
DavidStahl97 <david.stahl97@outlook.com>
2021-02-02 11:11:14 +0100

    InputWatcher component

---
+++ PizzaBlazor/Client/Pages/InputWatcher.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaBlazor.Client.Pages
+{
+    public class InputWatcher : ComponentBase
+    {
+        private EditContext _editContext;
+
+        [CascadingParameter]
+        public EditContext EditContext
+        {
+            get => _editContext;
+            set
+            {
+                _editContext = value;
+                EditContext.OnFieldChanged += async (sender, e) => await FieldChanged.InvokeAsync(e.FieldIdentifier.FieldName);
+            }
+        }
+
+        [Parameter]
+        public EventCallback<string> FieldChanged { get; set; }
+
+        public bool Validate() => EditContext?.Validate() ?? false;
+    }
+}
